dc02bbb58d49b18265cbc117f572a8c72f523d2e
Christophe Baillet <christophe.baillet@le-campus-numerique.fr>
2022-09-15 11:46:25 +0200

    Intégration de l'HTML de l'ancien Ecommerce

--- Ecommerce_Laravel/resources/views/cart.blade.php
+++ Ecommerce_Laravel/resources/views/cart.blade.php
@@ -1,1 +1,71 @@
+@extends('layout')
+@section('content')
 <h1>Panier</h1>
+<table class="table table-dark table-striped">
+    <tr>
+        <td>Nom du produit</td>
+        <td>Prix Unitaire</td>
+        <td>Quantité</td>
+        <td>Total</td>
+    </tr>
+    <tr>
+        <td>Produit</td>
+        <td>1€</td>
+        <td>2</td>
+        <td>1 €</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td>Total :</td>
+        <td>1 €</td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td></td>
+        <td>Total HT :</td>
+        <td>1€</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td>TVA :</td>
+        <td>20%</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>
+            <form method="post">
+                <select name="transporteur" id="select_transporteur">
+                    <option value="La_poste">La poste
+                    </option>
+                    <option value="Amazon">Amazon
+                    </option>
+                    <option value="daron">Mon daron
+                    </option>
+                    <option value="Musk">Elon Musk qui envoi ton colis sur Mars
+                    </option>
+                </select>
+                <input type="submit" value="VALIDER" class="btn btn-light">
+            </form>
+        </td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td>Prix transport :</td>
+        <td>1 €</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td>Total TTC :</td>
+        <td>1 €</td>
+    </tr>
+</table>
+<a href="product" class="btn btn-primary">Return</a>
+<a href="#" class="btn btn-success">Valider la commande</a>
+@endsection
---
+++ Ecommerce_Laravel/resources/views/includes/header.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar bg-light">
+    <div class="container-fluid d-flex justify-between">
+        <a class="navbar-brand" href="/">
+            <img src="https://www.pngitem.com/pimgs/m/117-1171768_transparent-cool-pngs-cool-logo-png-png-download.png" width="50" height="40">
+            Commerce
+        </a>
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a href="/cart" class="nav-link active" aria-current="page">Panier</a></li>
+        </ul>
+    </div>
+</nav>
